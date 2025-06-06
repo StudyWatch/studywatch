d575885b109f07b2696101b35d0d125abbed93e5
Timor malul <timor34@gmail.com>
2025-06-06 08:13:40 +0300

    ⚙️ Fix: Mark axios as external in Rollup to fix Vercel build

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['axios'] // מונע מ־Rollup לנסות "לפתור" את axios בקומפילציה
+    }
+  }
 });
